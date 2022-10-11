f679e655a4415ee5d75bea6ad4583918b190790e
Jasper Paterson <jasperpato@gmail.com>
2022-10-11 15:06:39 +0800

    commit

--- src/percolate.c
+++ src/percolate.c
@@ -359,7 +359,9 @@ int main(int argc, char *argv[])
     }
     if(nt_workers > 1) join_clusters(sites, b, n, nt_workers, p_start, np_rows);
     if(rank > MASTER) send_clusters(rank, sites, n, nt_workers, t_clusters, nt_clusters, p_start, p_end);
-    else if(rank == MASTER && n_workers > 1) { // receive cluster data
+
+    // receive cluster data
+    else if(rank == MASTER && n_workers > 1) { 
       int nc_attrs = 4 + 2*n; // number of ints that describes a cluster
       int p_stats[n_workers-1][4]; // num clusters, max cluster size, col perc, num border clusters
       int **data = calloc(n_workers-1, sizeof(int*));
@@ -373,7 +375,7 @@ int main(int argc, char *argv[])
       Cluster*** p_clusters = calloc(n_workers, sizeof(Cluster**));
       int* np_clusters = calloc(n_workers, sizeof(int));
       for(int i = 0; i < n_workers; ++i) p_clusters[i] = calloc(max_clusters(n, get_n_rows(n, i, n_workers)), sizeof(Cluster*));
-
+      printf("Hmm\n");
       // condense master clusters into array
       for(int tid = 0; tid < nt_workers; ++tid) {
         for(int i = 0; i < nt_clusters[tid]; ++i) {
@@ -381,18 +383,20 @@ int main(int argc, char *argv[])
           if(c->id != -1) p_clusters[0][np_clusters[0]++] = c;
         }
       }
+      printf("Him\n");
       // convert worker data into clusters and add to array
       for(int i = 0; i < n_workers-1; ++i) {
         int d_size = 2*n + nc_attrs*p_stats[i][3];
         for(int j = 2*n; j < d_size; j+=nc_attrs) { // loop through process clusters
-          Cluster *c = p_clusters[i+1][np_clusters[i+1]++];
-          c = calloc(1, sizeof(Cluster));
+          Cluster *c = calloc(1, sizeof(Cluster));
           c->rows = calloc(n, sizeof(short)); c->cols = calloc(n, sizeof(short));
           c->id = data[i][j]; c->size = data[i][j+1]; c->width = data[i][j+2]; c->height = data[i][j+3];
           for(int k = 0; k < n; ++k) c->rows[k] = data[i][j+4+k];
           for(int k = 0; k < n; ++k) c->cols[k] = data[i][j+n+4+k];
+          p_clusters[i+1][np_clusters[i+1]++] = c;
         }
       }
+      printf("Hah\n");
       // add site cluster pointers to sites
       for(int i = 1; i < n_workers; ++i) {
         int p_start = get_start(n, n, i, n_workers);
