b5dfc45569313df9dbaaf879d85c777e357c65ed
Jasper Paterson <jasperpato@gmail.com>
2022-10-13 13:44:05 +0800

    commit

--- test/driver.c
+++ test/driver.c
@@ -14,22 +14,7 @@
 #include <errno.h>
 #include <time.h>
 
-#define N_MIN 500
-#define N_MAX 1000
-#define N_STEP 500
-
-#define P_MIN 0.2
-#define P_MAX 1.0
-#define P_STEP 0.2
-
-#define P_RES (int)1e3
-
-#define NT_MIN 2
-#define NT_MAX 8
-#define NT_STEP 2
-
 #define ARG_LENGTH 40
-
 #define ONAME "../test/results.csv"
 
 /**
@@ -38,31 +23,35 @@
  * Loops through n, p n_cpus and n_threads values and calls percolate program infinitely
  */
 int main(int argc, char *argv[]) {
+
+  int ns[] = {1000,2000,3000,4000,5000,6000}, n_size = 6;
+  float ps[] = {0.2,0.4,0.8}; int p_size = 3;
+  int nts[] = {1,4,8}, nt_size = 3;
+
   srand(time(NULL));
   while(1) {
-    for(int n = N_MIN; n <= N_MAX; n+=N_STEP) {
-      for(int pi = (int)(P_MIN*P_RES); pi <= (int)(P_MAX*P_RES); pi+=(int)(P_STEP*P_RES)) {
-        float p = (float)pi/P_RES;
+    for(int n = 0; n < n_size; ++n) {
+      for(int p = 0; p < p_size; ++p) {
         int seed = rand(); // same seed for each n_cpus and n_threads to make sure the results are identical
-        for(int n_threads = NT_MIN; n_threads <= NT_MAX; n_threads+=NT_STEP) {
+        for(int nt = 0; nt < nt_size; ++nt) {
           int pid = fork();
           if(pid == -1) exit(EXIT_FAILURE);
           else if(pid == 0) { // child
-            char *args[] = {"percolate", "-r", NULL, "-o", NULL, NULL, NULL, NULL, NULL};
-            args[2] = malloc(ARG_LENGTH*sizeof(char)); sprintf(args[2], "%d", seed); // same seed for all n_threads
-            args[4] = malloc(ARG_LENGTH*sizeof(char)); sprintf(args[4], "%s", ONAME);
-            args[5] = malloc(ARG_LENGTH*sizeof(char)); sprintf(args[5], "%d", n);
-            args[6] = malloc(ARG_LENGTH*sizeof(char)); sprintf(args[6], "%f", p);
-            args[7] = malloc(ARG_LENGTH*sizeof(char)); sprintf(args[7], "%d", n_threads);
-            execv("../src/percolate", args);
+            // char *args[] = {"percolate", "-r", NULL, "-o", NULL, NULL, NULL, NULL, NULL};
+            // args[2] = malloc(ARG_LENGTH*sizeof(char)); sprintf(args[2], "%d", seed); // same seed for all n_threads
+            // args[4] = malloc(ARG_LENGTH*sizeof(char)); sprintf(args[4], "%s", ONAME);
+            // args[5] = malloc(ARG_LENGTH*sizeof(char)); sprintf(args[5], "%d", ns[n]);
+            // args[6] = malloc(ARG_LENGTH*sizeof(char)); sprintf(args[6], "%f", ps[p]);
+            // args[7] = malloc(ARG_LENGTH*sizeof(char)); sprintf(args[7], "%d", nts[nt]);
+            // execv("../src/percolate", args);
 
-            // char *args[] = {"--mpi=pmix", "../src/percolate", "-r", NULL, "-o", NULL, NULL, NULL, NULL, NULL};
-            // args[3] = malloc(ARG_LENGTH*sizeof(char)); sprintf(args[3], "%d", seed); // same seed for all n_threads
-            // args[5] = malloc(ARG_LENGTH*sizeof(char)); sprintf(args[5], "%s", ONAME);
-            // args[6] = malloc(ARG_LENGTH*sizeof(char)); sprintf(args[6], "%d", n);
-            // args[7] = malloc(ARG_LENGTH*sizeof(char)); sprintf(args[7], "%f", p);
-            // args[8] = malloc(ARG_LENGTH*sizeof(char)); sprintf(args[8], "%d", n_threads);
-            // execv("srun", args);
+            char *args[] = {"--mpi=pmix", "../src/percolate", "-r", NULL, "-o", NULL, NULL, NULL, NULL, NULL};
+            args[3] = malloc(ARG_LENGTH*sizeof(char)); sprintf(args[3], "%d", seed); // same seed for all n_threads
+            args[5] = malloc(ARG_LENGTH*sizeof(char)); sprintf(args[5], "%s", ONAME);
+            args[6] = malloc(ARG_LENGTH*sizeof(char)); sprintf(args[6], "%d", ns[n]);
+            args[7] = malloc(ARG_LENGTH*sizeof(char)); sprintf(args[7], "%f", ps[p]);
+            args[8] = malloc(ARG_LENGTH*sizeof(char)); sprintf(args[8], "%d", nts[nt]);
+            execv("srun", args);
           }
           else { // parent
             int status;
