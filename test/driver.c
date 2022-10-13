6bd503f0f11aa080945e77ee9724b045240d7fd6
Jasper Paterson <jasperpato@gmail.com>
2022-10-13 09:34:59 +0800

    commit

--- test/driver.c
+++ test/driver.c
@@ -14,25 +14,23 @@
 #include <errno.h>
 #include <time.h>
 
-#define N_MIN 100
-#define N_MAX 3000
-#define N_STEP 100
+#define N_MIN 500
+#define N_MAX 1000
+#define N_STEP 500
 
-#define P_MIN 0.0
+#define P_MIN 0.2
 #define P_MAX 1.0
-#define P_STEP 0.1
+#define P_STEP 0.2
 
 #define P_RES (int)1e3
 
-#define NC_MIN 1
-#define NC_MAX 4
-#define NC_STEP 1
-
-#define NT_MIN 1
-#define NT_MAX 24
-#define NT_STEP 1
+#define NT_MIN 2
+#define NT_MAX 8
+#define NT_STEP 2
 
 #define ARG_LENGTH 40
+
+#define ONAME "../test/results.csv"
 
 /**
  * USAGE: ./driver [ >> RESULTS_FILE ] (pipe csv results from stdout to file)
@@ -46,25 +44,22 @@ int main(int argc, char *argv[]) {
       for(int pi = (int)(P_MIN*P_RES); pi <= (int)(P_MAX*P_RES); pi+=(int)(P_STEP*P_RES)) {
         float p = (float)pi/P_RES;
         int seed = rand(); // same seed for each n_cpus and n_threads to make sure the results are identical
-        for(int n_cpus = NC_MIN; n_cpus <= NC_MAX; n_cpus += NC_STEP) {
-          for(int n_threads = NT_MIN; n_threads <= NT_MAX; n_threads+=NT_STEP) {
-            int pid = fork();
-            if(pid == -1) exit(EXIT_FAILURE);
-            else if(pid == 0) { // child
-              char *args[] = {"percolate", "-r", NULL, NULL, NULL, NULL, NULL, NULL};
-              for(int i = 2; i < 7; ++i) args[i] = malloc(ARG_LENGTH*sizeof(char));
-              sprintf(args[2], "%d", seed); // same seed for all n_threads
-              sprintf(args[3], "%d", n);
-              sprintf(args[4], "%f", p);
-              sprintf(args[5], "%d", n_cpus);
-              sprintf(args[6], "%d", n_threads);
-              execv("../src/percolate", args);
-            }
-            else { // parent
-              int status;
-              wait(&status); // wait for child
-              if (!WIFEXITED(status)) printf("%s\n", strerror(status));
-            }
+        for(int n_threads = NT_MIN; n_threads <= NT_MAX; n_threads+=NT_STEP) {
+          int pid = fork();
+          if(pid == -1) exit(EXIT_FAILURE);
+          else if(pid == 0) { // child
+            char *args[] = {"srun", "--mpi-pmix", "./percolate", "-r", NULL, "-o", NULL, NULL, NULL, NULL, NULL};
+            args[4] = malloc(ARG_LENGTH*sizeof(char)); sprintf(args[4], "%d", seed); // same seed for all n_threads
+            args[6] = malloc(ARG_LENGTH*sizeof(char)); sprintf(args[6], "%s", ONAME);
+            args[7] = malloc(ARG_LENGTH*sizeof(char)); sprintf(args[6], "%d", n);
+            args[8] = malloc(ARG_LENGTH*sizeof(char)); sprintf(args[7], "%f", p);
+            args[9] = malloc(ARG_LENGTH*sizeof(char)); sprintf(args[9], "%d", n_threads);
+            execv("../src/percolate", args);
+          }
+          else { // parent
+            int status;
+            wait(&status); // wait for child
+            if (!WIFEXITED(status)) printf("%s\n", strerror(status));
           }
         }
       }
