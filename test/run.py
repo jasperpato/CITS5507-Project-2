5e5b68600083a2d2839abbfd60037d4d63538bf7
Jasper Paterson <jasperpato@gmail.com>
2022-10-18 21:15:53 +0800

    commit

--- test/run.py
+++ test/run.py
@@ -2,4 +2,5 @@ from subprocess import run
 from sys import argv
 
 if __name__ == '__main__':
+  print('Ok')
   print(run(['srun', '--mpi=pmix', './percolate', *argv[1:]], capture_output=True).stdout.decode('utf-8'))
