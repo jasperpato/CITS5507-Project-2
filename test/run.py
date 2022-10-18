feb5a3dfde14e2966d813eb6f06fe27cb8a68548
Jasper Paterson <jasperpato@gmail.com>
2022-10-18 21:31:21 +0800

    commit

--- test/run.py
+++ test/run.py
@@ -2,4 +2,6 @@ from subprocess import run
 from sys import argv
 
 if __name__ == '__main__':
-  print(run(['srun', '--mpi=pmix', '--nodes=1', '--ntasks-per-node=1', '../src/percolate', *[a for a in argv[1:] if 'nodes' not in a]], capture_output=True).stdout.decode('utf-8'), end='')
+  args = ['srun', '--mpi=pmix', '--nodes=1', '--ntasks-per-node=1', '../src/percolate', *[a for a in argv[1:] if 'nodes' not in a]]
+  print(args)
+  print(run(args, capture_output=True).stdout.decode('utf-8'), end='')
