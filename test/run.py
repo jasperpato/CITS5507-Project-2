552b98b26afbe9de9ad0474b1b16a5ba9e96dd8d
Jasper Paterson <jasperpato@gmail.com>
2022-10-18 21:16:30 +0800

    commit

--- test/run.py
+++ test/run.py
@@ -3,4 +3,4 @@ from sys import argv
 
 if __name__ == '__main__':
   print('Ok')
-  print(run(['srun', '--mpi=pmix', './percolate', *argv[1:]], capture_output=True).stdout.decode('utf-8'))
+  print(run(['srun', '--mpi=pmix', '../src/percolate', *argv[1:]], capture_output=True).stdout.decode('utf-8'))
