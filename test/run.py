f45ff9beb5b6f33ba8cc9369318f927e52043b22
Jasper Paterson <jasperpato@gmail.com>
2022-10-18 21:46:23 +0800

    commit

--- test/run.py
+++ test/run.py
@@ -5,11 +5,13 @@ ps = [0.3, 0.7]
 ncs = [1, 2]
 nts = [1]
 
+seed = 10
+
 if __name__ == '__main__':
   
   for n in ns:
     for p in ps:
       for nc in ncs:
         for nt in nts:
-          args = ['srun', '--mpi=pmix', f'--nodes={nc}', f'--ntasks-per-node={nt}', '../src/percolate', str(n), str(p), str(nt)]
+          args = ['srun', '--mpi=pmix', f'--nodes={nc}', f'--ntasks-per-node={nt}', '../src/percolate', '-r', str(seed), str(n), str(p), str(nt)]
           print(run(args, capture_output=True).stdout.decode('utf-8'), end='')
