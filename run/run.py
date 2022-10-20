f4c8d1be6640f3a00c6bded607fbd24d741f4190
Jasper Paterson <jasperpato@gmail.com>
2022-10-20 21:26:19 +0800

    commit

--- run/run.py
+++ run/run.py
@@ -10,16 +10,17 @@ LOOPS = 1
 if __name__ == '__main__':
   l = 0
   random.seed()
-  with open('results.csv', 'a') as f:
-    while l < LOOPS:
-      for n in ns:
-        for p in ps:
-          r = random.randint(0,10000)
-          for nc in ncs:
-            for nt in nts:
-              args = ['srun', '--mpi=pmix', f'--nodes={nc}', f'--ntasks-per-node={nt}', '../src/percolate', '-r', str(r), str(n), str(p), str(nt)]
-              # args = ['../src/percolate', '-r', str(r), str(n), str(p), str(nt)]
-              out = subprocess.run(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-              print(out.stdout.decode('utf-8'))
-              # print(out.stderr.decode('utf-8'), end='')
-              l += 1
+  # with open('results.csv', 'a') as f:
+  while l < LOOPS:
+    for n in ns:
+      for p in ps:
+        r = random.randint(0,10000)
+        for nc in ncs:
+          for nt in nts:
+            # args = ['srun', '--mpi=pmix', f'--nodes={nc}', f'--ntasks-per-node={nt}', '../src/percolate', '-r', str(r), str(n), str(p), str(nt)]
+            args = ['srun', '--mpi=pmix', '../src/percolate', '-r', str(r), str(n), str(p), str(nt)]
+            # args = ['../src/percolate', '-r', str(r), str(n), str(p), str(nt)]
+            out = subprocess.run(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            print(out.stdout.decode('utf-8'), end='')
+            # print(out.stderr.decode('utf-8'), end='')
+            l += 1
