99b90e365acf1c1101066624bf74ecd1ced1d0eb
Jasper Paterson <jasperpato@gmail.com>
2022-10-25 14:11:19 +0800

    graph

--- run/graph.py
+++ run/graph.py
@@ -17,7 +17,8 @@ from argparse import ArgumentParser
 P_RES = 1e-3
 
 ncs = [1, 2, 3, 4]
-file = '../results/results{}.csv'
+rfile = '../results/results{}.csv'
+sfile = '../graph/graph_{}{}{}{}{}.png'
 
 '''
 Returns a list of dictionaries - one for each row in csvfile
@@ -36,13 +37,13 @@ def read(fname):
 '''
 Return data mapping x to mean total time
 '''    
-def average(data):
+def average(data, verbose=False):
   lens = []
   for d in data.values():
     for x, times in d.items():
       lens.append(len(times))
       d[x] = np.mean(times) if times else 0
-  print(f'Min data points per parameter set: {min(lens) if lens else 0}')
+  if verbose: print(f'Min data points per parameter set: {min(lens) if lens else 0}')
 
 '''
 Gather data from rows that match the cval (cname could be n or p)
@@ -64,8 +65,9 @@ def get_data(fname, cname, cval, group, ncpus, nthreads):
 '''
 Graph all n_threads on one axis, keeping either n or p constant
 '''
-def graph(data, cname, cval, group, nsquared):
+def graph(data, cname, cval, group, nsquared, save=False, file='', cs=None, ts=None):
 
+  plt.figure(figsize=(8, 6))
   for t, d in data.items():
     zs = sorted(((k**2, v) for k, v in d.items()) if nsquared and cname == 'p' else d.items())
     xs, ys = [z[0] for z in zs], [z[1] for z in zs]
@@ -80,7 +82,13 @@ def graph(data, cname, cval, group, nsquared):
   labels, handles = zip(*sorted(zip(labels, handles), key=lambda t: t[0]))
   plt.legend(handles, labels, title=str(group))
   
-  plt.show(block=True)
+  if save:
+    cval = f'0{int(cval*100)}' if cname == 'p' else cval
+    c = '_c' + ''.join(map(lambda x: f'-{x}', cs)) if cs else ''
+    t = '_t' + ''.join(map(lambda x: f'-{x}', ts)) if ts else ''
+    plt.savefig(file.format(cname, cval, c, t, '_ns' if nsquared else ''))
+
+  else: plt.show(block=True)
 
 if __name__ == '__main__':
   a = ArgumentParser()
@@ -88,17 +96,35 @@ if __name__ == '__main__':
   a.add_argument('-p', type=float, default=0.4)
   a.add_argument('-t')
   a.add_argument('-c')
+  a.add_argument('--all', action='store_true')
   a.add_argument('--n-squared', action='store_true')
+  a.add_argument('--save', action='store_true')
   args = vars(a.parse_args())
   
-  cname = 'n' if args['n'] else 'p'
-  cval = args[cname]
   group = ('ncpus', 'nthreads')
 
-  ncpus    = [int(c) for c in args['c'].split(',')] if args['c'] else None
-  nthreads = [int(t) for t in args['t'].split(',')] if args['t'] else None
+  if args['all']:
+    
+    # all sets p nsquared
+    for p in (0.2, 0.4, 0.6, 0.8):
+      ncpus, nthreads = None, None
+      data = get_data(rfile, 'p', p, group, ncpus, nthreads)
+      graph(data, 'p', p, group, True, True, sfile, ncpus, nthreads)
 
-  data = get_data(file, cname, cval, group, ncpus, nthreads)
-  graph(data, cname, cval, group, args['n_squared'])
+    # all sets n
+    for n in (1000, 2000, 4000, 5000):
+      ncpus, nthreads = None, None
+      data = get_data(rfile, 'n', n, group, ncpus, nthreads)
+      graph(data, 'n', n, group, False, True, sfile, ncpus, nthreads)
+
+  else:
+    cname = 'n' if args['n'] else 'p'
+    cval = args[cname]
+
+    ncpus    = [int(c) for c in args['c'].split(',')] if args['c'] else None
+    nthreads = [int(t) for t in args['t'].split(',')] if args['t'] else None
+
+    data = get_data(rfile, cname, cval, group, ncpus, nthreads)
+    graph(data, cname, cval, group, args['n_squared'], args['save'], sfile, ncpus, nthreads)
 
   
