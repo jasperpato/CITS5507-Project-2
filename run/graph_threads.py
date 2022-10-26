08ea68e65dcb3c6394ccc26fdcfaac27dddd6e93
Jasper Paterson <jasperpato@gmail.com>
2022-10-26 13:52:06 +0800

    ok

--- run/graph_threads.py
+++ run/graph_threads.py
@@ -1,5 +1,6 @@
 from graph import read
 import matplotlib.pyplot as plt
+from pprint import pprint
 
 rows = read('../results/threads4.csv')
 
@@ -13,14 +14,17 @@ for r in rows:
 for nt in d:
   d[nt] = sum(d[nt]) / len(d[nt])
 
+pprint(d)
+
 plt.figure(figsize=(8, 6))
 zs = sorted(d.items())
 xs, ys = [z[0] for z in zs], [z[1] for z in zs]
 plt.plot(xs, ys)
+plt.xticks(range(8, 28+1, 2))
 
 plt.xlabel('N_THREADS')
 plt.ylabel('Mean total time (s)')
-plt.title('N = 5000 and P = 0.3')
+plt.title('N_NODES = 4, N = 5000, P = 0.3')
 
 # plt.show(block=True)
 
